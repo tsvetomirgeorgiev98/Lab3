eea42a4a1af9b26ab867af784c8b85420e0d33b0
tsvetomirgeorgiev98 <1705848@rgu.ac.uk>
2021-02-04 16:53:48 +0000

    anges

--- server.js
+++ server.js
@@ -5,23 +5,24 @@ const PORT = process.env.PORT || 8080;
 var port = PORT;
 // send our index.html file to the user for the home page
 app.get('/', function(req, res) {
+  // create routes for the admin section
+  //get an instance of the router
+  var adminRouter = express.Router();
+  // admin main page. the dashboard (http://localhost:PORT/admin)
+  adminRouter.get('/', function(req, res) {
+   res.send('I am the dashboard!'); });
+  // users page (http://localhost:PORT/admin/users)
+  adminRouter.get('/users', function(req, res) {
+   res.send('I show all the users!'); });
+  // posts page (http://localhost:PORT/admin/posts)
+  adminRouter.get('/posts', function(req, res) {
+   res.send('I show all the posts!'); });
+  // apply the routes to our application
+  app.use('/admin', adminRouter); 
  res.sendFile(__dirname + '/index.html');
 });
 
-// create routes for the admin section
-//get an instance of the router
-var adminRouter = express.Router();
-// admin main page. the dashboard (http://localhost:PORT/admin)
-adminRouter.get('/', function(req, res) {
- res.send('I am the dashboard!'); });
-// users page (http://localhost:PORT/admin/users)
-adminRouter.get('/users', function(req, res) {
- res.send('I show all the users!'); });
-// posts page (http://localhost:PORT/admin/posts)
-adminRouter.get('/posts', function(req, res) {
- res.send('I show all the posts!'); });
-// apply the routes to our application
-app.use('/admin', adminRouter); 
+
 
 // start the server
 app.listen(PORT);
