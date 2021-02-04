ba6930663404a2c7642fa077c52d69d90bea5fa3
tsvetomirgeorgiev98 <1705848@rgu.ac.uk>
2021-02-04 16:20:25 +0000

    port variable

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var http = require("http");
 
+const PORT = process.env.PORT || 3000;
+
 http.createServer(function (request, response){
 
   response.writeHead(200, {'Content-Type': 'text/plain'});
 
   response.end('Hello World\n');
-}).listen(process.env.PORT || 5000);
-
-console.log('Server running at http://127.0.0.1:8080/');
+}).listen(PORT, () => {
+ console.log(`Our app is running on port ${ PORT }`);
+});
