5f911930ce4659e14a3301d4b2ee6e947b8e72a6
tsvetomirgeorgiev98 <1705848@rgu.ac.uk>
2021-02-04 16:39:03 +0000

    changes

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var http = require("http");
-
+fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
 http.createServer(function (request, response){
 
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Access-Control-Allow-Origin' : '*'
+  });
 
-  response.end('Hello World\n');
+  var readStream = fs.createReadStream(__dirname + '/index.html');
+
+  readStream.pipe(response);
 }).listen(PORT, () => {
  console.log(`Our app is running on port ${ PORT }`);
 });
